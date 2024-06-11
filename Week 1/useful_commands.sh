0898295a74f26f93a76a87ba967070bb7971715c
Cristian Andrés Villarreal Orozco <147870401+cristianvo74@users.noreply.github.com>
2024-06-11 05:22:34 +0000

    This is the first Week

---
+++ Week 1/useful_commands.sh
@@ -0,0 +1,34 @@
+#Time and date
+cal
+date
+
+#Disk usage or file size
+df -h
+du -sh
+
+#Exploring
+pwd #working path
+ls -lah # 
+cd /tmp # go temp files
+cd ~  # go home
+which python3   # python path executable
+
+
+#Viewing files
+less #Display the file
+cat  #print the whole file
+
+
+#Counting lines
+wc -l
+
+#Modifying files and directories
+touch # creates a file if it doesn't exist
+mkdir # create a new directory
+mkdir moredir/dir2/dir1 # create multiple directories
+mv # move file
+rm -rf # remove a directory
+
+Processes
+ps # shows processes running
+
